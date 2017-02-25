a32f2c46850a0ea7ed450497cebe456b13582ad2
Sean Rose <sean1rose@gmail.com>
2017-02-24 21:11:30 -0800

    testing remove mongoose methods

---
+++ test/delete_test.js
@@ -0,0 +1,60 @@
+const assert = require('assert');
+const User = require('../src/user');
+
+describe('Deleting a user', () => {
+  // add before each to create a new user, joe and save to db. so in each it block, will remove that instance of joe
+  let joe;
+
+  beforeEach((done) => {
+    joe = new User({ name: 'Joe' });
+    joe.save()
+      .then(() => done());
+  });
+
+  // this is on the SINGLE joe model INSTANCE (as opposed to entire User class). remove 1 specific instance.
+  it('model instance remove', (done) => {
+    // testing remove function...
+    // remove joe -> then callback -> then search thru collection looking for joe -> then callback -> then whatever user is returned from that search, assert it doesnt exist (cuz he was deleted)
+      // 2 promises executed sequentially.
+    joe.remove()
+      .then(() => {
+        // 1st then is promise returned from running the remove method...
+        User.findOne({ name: 'Joe' })
+          .then((user) => {
+            // 2nd then is w/ the user that was found w/ the findOne method (or lack thereof)
+            assert(user === null);
+            done();
+          })
+      });
+      /* THIS IS ALTERNATIVE CODE TO THE ABOVE ^^^...
+      .then(() => User.findOne({ name: 'Joe' }))
+      .then((user) => {
+        assert(user === null);
+        done();
+      })
+      */
+  });
+
+  // this is on entire User class...
+  it('class method remove', (done) => {
+    // use this when you want to remove a BUNCH of records w/ some given criteria. METHOD ON THE CLASS (not a single instance)
+    User.remove({ name: 'Joe' })
+      .then(() => {
+        User.find({name: 'Joe'})
+          .then((usersArray) => {
+            assert(usersArray.length === 0);
+            done();
+          });
+      });
+
+  });
+
+  
+  it('class method findAndRemove', () => {
+
+  });
+
+  it ('class method findByIdAndRemove', () => {
+
+  });
+});
--- test/reading_test.js
+++ test/reading_test.js
@@ -17,20 +17,33 @@ describe('Reading users out of the db', () => {
 
   // finding all the users w/ name sean...
     // since it's an asyncrhonous test (will take time to complete) -> must use done callback
+    // FIND -> RETURNS AN ARRAY OF RECORDS
   it('finds all users w/ a name of Sean', (done) => {
     // query db to find a list of all users w/ name of sean
     // use mongo class-method
       // User.find(criteria) returns an array vs User.findOne(criteria) returns the first record that matches
     User.find({name: 'Sean'})
-      .then((usersArray) => {
+      .then((users) => {
         // assert that user is the same user as the one we inserted beforeeach... Use _id property....
         
         // console.log('sean._id - ', sean._id);
-        // console.log('array[0] - ', usersArray[0]._id);
+        // console.log('array[0] - ', users[0]._id);
         
-        // ^^^ the _ids match, but need to use 'equals()' method OR use toString() method cuz the _id's are object ids
-        // assert(sean._id.equals(usersArray[0]._id));
-        assert(sean._id.toString() === usersArray[0]._id.toString());
+        // ^^^ the _ids match, but need to use 'equals()' method OR use toString() method cuz the _id's are object ids and doing a comparison
+        // assert(sean._id.equals(users[0]._id));
+        assert(sean._id.toString() === users[0]._id.toString());
+        done();
+      });
+  });
+
+  // find one user w/ id 
+    // FINDONE -> returns a single record
+  it('find a user w/ a particular id', (done) => {
+    // need to pass in object to serve as criteria of your findOne search...
+    User.findOne({ _id: sean._id})
+      .then((user) => {
+        console.log('user - ', user);
+        assert(user.name === 'Sean');
         done();
       });
   });
